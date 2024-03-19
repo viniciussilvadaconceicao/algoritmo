aff1776c9066eee1223a792785bd2d83722b924f
VINICIUS SILVA <163214766+viniciussilvadaconceicao@users.noreply.github.com>
2024-03-18 21:34:30 -0300

    Delete jogo_5(ano_bissexto).py

--- jogo_5(ano_bissexto).py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
-ano = int(input('digite o ano:'))
-calculo = ano % 4
-if calculo == 0:
-  print(f'o ano {ano}, é bisesexto')
-else:
-  print(f'o ano {ano}, não é bissexto')
