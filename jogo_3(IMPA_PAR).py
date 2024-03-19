8064f93ba85c14a6ecb1294e94ae5781a35b89d3
VINICIUS SILVA <163214766+viniciussilvadaconceicao@users.noreply.github.com>
2024-03-18 21:33:49 -0300

    Delete jogo_3(IMPA_PAR).py

--- jogo_3(IMPA_PAR).py
+++
@@ -1,6 +0,0 @@
-num = int(input('digite um numero:'))
-resultado = num % 2 #qualquer numero sendo impa da 1 e par da 0
-if resultado == 0:
-     print(f'o numero: {num} é PAR')
-else:
-     print(f'o numero: {num} é IMPA')
