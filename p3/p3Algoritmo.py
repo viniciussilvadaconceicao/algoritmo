b6a716158029d9a918b35a85976224563a2b05b8
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-09 20:02:04 -0300

    Update p3Algoritmo.py

--- p3/p3Algoritmo.py
+++ p3/p3Algoritmo.py
@@ -1,3 +1,84 @@
+def estudante():
+    from time import sleep
+    print('='*80)
+    print('''Bem vindo(a) estudande, ao sitema da biblioteca digital universitária.
+escolha o numero da opção que deseja acessar: 
+          
+ATENÇÃO: lembre que para acessar uma das opções abaixo precisa digitar um numero:
+          
+          [1] - livros
+          [2] - artigos científicos
+          [3] - teses 
+          [4] - retorna
+          ''')
+    print('='*80)
+    try:
+        opcao = int(input("digite o numero da opção desejada: "))
+        if opcao == 1:
+            limpar_tela()
+            lista_livros()
+            sleep(8)
+
+        elif opcao == 2:
+            limpar_tela()
+            lista_artigos()
+            sleep(8)
+
+        elif opcao == 3:
+            limpar_tela()
+            lista_teses()
+            sleep(8)
+
+        elif opcao == 4:
+            return menu_principal()
+        
+    except ValueError:
+        print('ERRO, escolha uma das opçoes digitando o numero correspondente')
+
+def lista_teses():
+    from time import sleep
+    try:
+        with open('tese.txt', 'r') as arquivo:
+            tese = arquivo.readlines()
+            tese = [tese.strip().split(',') for tese in tese]
+            tese_ordenados = bubble_sort(tese)
+            for tese in tese_ordenados:
+                print(tese)
+                sleep(5)
+            print('Programa ira retornar ')
+            sleep(3)
+            return menu_principal()
+    except IOError:
+        print('ERRO, livro não foi encontrado na lista')
+        sleep(3)
+        return menu_principal()
+    
+def lista_livros():
+    from time import sleep
+    try:
+        with open('livrosdigital.txt', 'r') as arquivo:
+            livros = arquivo.readlines()
+            livros = [livro.strip().split(',') for livro in livros]
+            livros_ordenados = bubble_sort(livros)
+            for livro in livros_ordenados:
+                print(livro)
+                sleep(5)
+            print('Programa ira retornar ')
+            sleep(3)
+            return menu_principal()
+    except IOError:
+        print('livro não esta na lista')
+        sleep(3)
+        return menu_principal()  
+
+def  bubble_sort(lista):
+    lendo_lista = len(lista)
+    for a in range(lendo_lista):
+        for b in range(lendo_lista - a - 1):
+            if lista[b] > lista[b + 1]:
+                lista[b], lista[b + 1] = lista[b + 1], lista[b]
+    return lista
+
 def limpar_tela():
     import os 
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -39,18 +120,19 @@ def cadastra_livros():
 def cadastra_teses():
     from time import sleep
     print('='*80)
-    print('Bem vindo ao cadastro de artigos científicos da biblioteca digital universitária. ')
+    print('Bem vindo ao cadastro de teses da biblioteca digital universitária. ')
     print('='*80)
     try:
-        artigo = str(input('digite o titulo do livro: '))
-        with open('artigo.txt', 'a') as arquivo:
-            arquivo.write(f'artigo:{artigo}\n')
+        tese = str(input('digite o nome da tese: '))
+        with open('tese.txt', 'a') as arquivo:
+            arquivo.write(f'tese:{tese}\n')
             print('Todas essas infomações foi inserida no sitema')
             sleep(3)
             return gerenciamento()
         
     except ValueError:
         print('ERRO, digite o ano de publicação do livro em numeros')
+
 def gerenciamento():
     print('='*80)
     print('''Bem vindo(a) caro bibliotecario(a) ao sitema de gerenciamento da biblioteca digital universitária.
@@ -67,12 +149,15 @@ ATENÇÃO: lembre que para acessar uma das opções abaixo precisa digitar um nu
     try:
         opcao = int(input("digite o numero da opção desejada: "))
         if opcao == 1:
+            limpar_tela()
             cadastra_livros()
 
         elif opcao == 2:
+            limpar_tela()
             cadastra_artigos()
 
         elif opcao == 3:
+            limpar_tela()
             cadastra_teses()
 
         elif opcao == 4:
